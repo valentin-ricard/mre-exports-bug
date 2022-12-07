0ddc6407a827941939935b597edba00b8dfc1c29
Valentin Ricard <91870700+valentin-ricard@users.noreply.github.com>
2022-12-07 15:47:03 +0000

    wip: Tenative changes number 2

--- library/src/main/java/com/example/application/TestComponent.java
+++ library/src/main/java/com/example/application/TestComponent.java
@@ -1,8 +1,11 @@
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @NpmPackage(value = "@praxo/mre-exports", version = "1.0.0")
 @JsModule("@praxo/mre-exports/other.js")
-public class TestComponent extends Component {
+@Tag("test")
+public class TestComponent extends LitTemplate {
 }
